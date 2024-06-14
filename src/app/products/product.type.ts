e6e6c940910552219725d958d21f320c8b779e19
Ostap Stasiuk <ostapstasyuk@gmail.com>
2024-06-14 21:28:03 +0300

    initial commit

---
+++ src/app/products/product.type.ts
@@ -0,0 +1,47 @@
+export type Shop = {
+  id: number,
+  name: string
+}
+
+export type StockStatus = 'INSTOCK' | 'OUTOFSTOCK' | 'LOWSTOCK'
+
+export type Product = {
+  id: string,
+  name: string,
+  sellable: boolean | null,
+  category: {
+    id: string,
+    name: string
+  },
+  unitOfMeasure: {
+    id: string,
+    name: string
+  },
+  stock: number,
+  price: number,
+  inventoryStatus: StockStatus,
+  dataPerShop: {
+    shop: Shop,
+    price: number,
+    stock: number,
+    inventoryStatus: StockStatus
+  }[]
+}
+
+export type ShopProduct = {
+  id: string,
+  name: string,
+  sellable: boolean | null,
+  category: {
+    id: string,
+    name: string
+  },
+  unitOfMeasure: {
+    id: string,
+    name: string
+  },
+  shop: Shop,
+  price: number,
+  stock: number,
+  inventoryStatus: StockStatus
+}
---
+++ src/app/products/products-list.component.ts
@@ -0,0 +1,67 @@
+import { ChangeDetectionStrategy, Component, Pipe, PipeTransform, computed, inject, signal } from '@angular/core';
+import { Table } from 'primeng/table';
+import { ProductApiService } from './product-api.service';
+import { PrimeNgImportsModule } from '../utils/prime-ng-imports';
+import { Product, Shop, StockStatus } from './product.type';
+import { shareReplay } from 'rxjs';
+
+@Pipe({
+  name: 'statusToSeverity',
+  standalone: true
+})
+export class StatusToSeverityPipe implements PipeTransform {
+  transform(item: StockStatus): "success" | "secondary" | "info" | "warning" | "danger" | "contrast" | undefined {
+    switch (item) {
+      case 'INSTOCK': return 'success';
+      case 'LOWSTOCK': return 'warning';
+      case 'OUTOFSTOCK': return 'danger';
+    }
+  }
+}
+
+@Component({
+  selector: 'admin-products-list',
+  templateUrl: 'products-list.component.html',
+  standalone: true,
+  imports: [PrimeNgImportsModule, StatusToSeverityPipe],
+  providers: [ProductApiService],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductsListComponent {
+  #productApi = inject(ProductApiService)
+  products$ = this.#productApi.fetchProducts().pipe(shareReplay(1))
+  products = signal<Product[]>(null)
+  loading = computed(() => !this.products())
+  shops$ = this.#productApi.fetchShops()
+  expandedRows = signal({});
+
+  constructor() {
+    this.#loadProducts()
+  }
+
+  statuses = signal<{ label: string, value: StockStatus }[]>([
+    { label: 'IN STOCK', value: 'INSTOCK' },
+    { label: 'LOW', value: 'LOWSTOCK' },
+    { label: 'OUT', value: 'OUTOFSTOCK' }
+  ]);
+
+  clear(table: Table) {
+    table.clear();
+  }
+
+  expandAll() {
+    this.expandedRows.set(this.products().reduce((acc, p) => (acc[p.id] = true) && acc, {}));
+  }
+
+  collapseAll() {
+    this.expandedRows.set({});
+  }
+
+  filterByShops(shops: Shop[]) {
+    this.#loadProducts(shops)
+  }
+
+  #loadProducts(shops?: Shop[]) {
+    this.#productApi.fetchProducts(shops).subscribe(products => this.products.set(products))
+  }
+}
