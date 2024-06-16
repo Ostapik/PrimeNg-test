0a1d59b55911d95d62c88c92d14d42795cfa0276
Ostap Stasiuk <ostapstasyuk@gmail.com>
2024-06-16 18:35:27 +0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductsListComponent } from './products/products-list.component';
-
-export const routes: Routes = [{
-  path: '',
-  component: ProductsListComponent
-}];
